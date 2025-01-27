23ca707928c1c8a164ac37f77dc6fe72d0c793b8
Catalin Peter <catalin.peter@pinteam.eu>
2025-01-27 12:24:21 +0200

    add new package

--- git_changes_detector.py
+++ git_changes_detector.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import sys
+import re
 
 def get_commit_data():
     """Gets commit data including changed files and diff content using GitHub API."""
@@ -29,24 +30,44 @@ def get_commit_data():
         print(f"Error retrieving commit data: {e}", file=sys.stderr)
         return [], ""
 
+def extract_package_name(package_path):
+    """Extracts the package name from the PACKAGE_PATH."""
+    path_parts = package_path.split(os.sep)
+    if len(path_parts) >= 3:
+        return f"{path_parts[-3]}_{path_parts[-2]}"
+    return ""
+
+def extract_package_version(diff_content):
+    """Extracts the package version from config.yaml diff."""
+    version_pattern = re.compile(r'\+\s+"(\d+\.\d+\.\d+)":')
+    match = version_pattern.search(diff_content)
+    return match.group(1) if match else ""
+
 def main():
     changed_files, diff_content = get_commit_data()
     
-    if changed_files:
-        package_path = " ".join(changed_files)  # Store the full absolute paths in PACKAGE_PATH
-        print(f"PACKAGE_PATH={package_path}")
+    # Filter PACKAGE_PATH to include only config.yml changes
+    config_files = [file for file in changed_files if file.endswith("config.yml")]
+    package_path = " ".join(config_files) if config_files else ""
+    
+    package_name = extract_package_name(config_files[0]) if config_files else ""
+    package_version = extract_package_version(diff_content) if config_files else ""
+    
+    print(f"PACKAGE_PATH={package_path}")
+    print(f"PACKAGE_NAME={package_name}")
+    print(f"PACKAGE_VERSION={package_version}")
 
-        # Export PACKAGE_PATH for use in subsequent GitHub Actions steps
-        github_env = os.getenv("GITHUB_ENV")
-        if github_env:
-            with open(github_env, "a") as env_file:
-                env_file.write(f"PACKAGE_PATH={package_path}\n")
-        
-        print("Changed files:")
-        for file in changed_files:
-            print(file)
-    else:
-        print("No files changed in the current commit.")
+    # Export variables for use in GitHub Actions
+    github_env = os.getenv("GITHUB_ENV")
+    if github_env:
+        with open(github_env, "a") as env_file:
+            env_file.write(f"PACKAGE_PATH={package_path}\n")
+            env_file.write(f"PACKAGE_NAME={package_name}\n")
+            env_file.write(f"PACKAGE_VERSION={package_version}\n")
+    
+    print("Changed files:")
+    for file in changed_files:
+        print(file)
     
     if diff_content:
         print("\nDiff content:")
